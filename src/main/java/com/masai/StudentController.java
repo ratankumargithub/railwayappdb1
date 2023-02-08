4dbb6616e1eae9a3c63023a36339054fec8a9bf8
Ratan <ratankapr22@gmail.com>
2023-02-08 15:17:59 +0530

    all http method

--- src/main/java/com/masai/StudentController.java
+++ src/main/java/com/masai/StudentController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +25,31 @@ public class StudentController {
 		this.sRepo = sRepo;
 	}
 
+	
+	
+	
+	@PostMapping("/post1")
+	public String postReq() {
+		return "from Post";
+	}
 
+	@PutMapping("/put1")
+	public String putReq() {
+		return "from Put";
+	}
 
+	@GetMapping("/get1")
+	public String getReq() {
+		return "from Get";
+	}
+
+	@DeleteMapping("/delete1")
+	public String deleteReq() {
+		return "from Delete";
+	}
+
+	
+	
 
 
 	@PostMapping("/st1")
