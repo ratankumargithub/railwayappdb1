97c5adf99f306f3f978f4c5655c6e1b91f06b30e
Ratan <ratankapr22@gmail.com>
2023-02-13 12:37:34 +0530

    swagger cfg

--- src/main/java/com/masai/Demo1Railwayappdb1Application.java
+++ src/main/java/com/masai/Demo1Railwayappdb1Application.java
@@ -1,7 +1,13 @@
 package com.masai;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 
 @SpringBootApplication
 public class Demo1Railwayappdb1Application {
@@ -10,4 +16,11 @@ public class Demo1Railwayappdb1Application {
 		SpringApplication.run(Demo1Railwayappdb1Application.class, args);
 	}
 
+	
+	@Bean
+	public OpenAPI customOpenAPI() {
+	    Server server = new Server();
+	    server.setUrl("https://testapp/api");
+	    return new OpenAPI().servers(List.of(server));
+	}
 }
